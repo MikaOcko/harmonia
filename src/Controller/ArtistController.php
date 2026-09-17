b36e6ffb7fc8b044aaee200ebd3b8911260d1838
Mika Ocko <moreau.magali.kimberly@gmail.com>
2026-09-17 12:01:21 +0200

    wip: add tracks to favorites, plus create atists controller and page

---
+++ src/Controller/ArtistController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArtistRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ArtistController extends AbstractController
+{
+    #[Route('/artist', name: 'app_artist')]
+    public function index(ArtistRepository $artistRepository): Response
+    {
+        $artists = $artistRepository->findAll();
+
+        return $this->render('artist/index.html.twig', [
+            'artists' => $artists,
+        ]);
+    }
+}
